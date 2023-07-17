e3ed9ed5ae4aa93717367399948d8f5b19ac89c9
LethaboAphane <lethabo_aphane@icloud.com>
2023-07-17 07:05:13 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,12 +12,13 @@ int main(void)
 	long long int d;
 	float f;
 
-printf("size of a char %lu byte(s)\n" , (unsigned long)sizeof(a));
-printf("size of an int %lu byte(s)\n" , (unsigned long)sizeof(b));
-printf("size of a long int %lu byte(s)\n" , (unsigned long)sizeof(c));
-printf("size of a long long int %lu byte(s)\n" , (unsigned long)sizeo (d));
-printf("size of a float %lu byte(s)\n" , (unsigned long)sizeof(f));
+printf("size of a char %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size an int %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size a long int %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size a long long int %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size a float %lu byte(s)\n", (unsigned long)sizeof(f));
+
 
 return (0);
-}
 
+}
