b1d9dbf08f382094d5e36de77d218037d7b82dda
LethaboAphane <lethabo_aphane@icloud.com>
2023-07-17 04:23:02 -0700

    hello.put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * return: Always 0 (sucess)
- *
+ * 
  */
 
-int main (void)
+int main(void)
 
-{ 
+{
 	puts("\"Programming is like building a multilingual puzzle);
 
 	return (0);
