005e7b5690d8dfdaba1f9eee7118335453c81d8a
LethaboAphane <lethabo_aphane@icloud.com>
2023-07-19 19:32:36 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,20 @@
 #include "main.h"
-#include <unistd.h>
+
 /**
- * print_alphabet - Entry point
- *  Return:0
+ * print_alphabet_x10 - Print lowercase alphabet 10 times
  */
-void print_alphabet(void)
 
+void print_alphabet_x10(void)
 {
+	char num, letter;
 
-        char c = 'a';
-        while (c <= 'z')
-     {
-             _putchar(c);
-                c++
-     }
-        _putchar('\n');
-
+	for (num = 0; num <= 9; num++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
 }
 
