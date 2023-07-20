fece4fa87df8ef5ae7ba4479a3d9438f8973af3c
LethaboAphane <lethabo_aphane@icloud.com>
2023-07-20 15:30:35 -0700

    main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h'
+/**
+ * main - test fuunction that prints if interger is positiveor negative
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int i;
+	i-98;
+	positive_or_negative(i);
+	return (0);
+}
+
