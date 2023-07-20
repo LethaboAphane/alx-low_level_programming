1f9044f9b7e26d5ef4ccbf4ac35a7e4b34871f25
LethaboAphane <lethabo_aphane@icloud.com>
2023-07-20 16:58:59 -0700

    deburgging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - causes an infinite lool
+ * main - causes an infinite loop
  * Return: 0
  */
 
@@ -9,10 +9,12 @@ int main(void)
 
 {
 	int i;
+	
 	printf("infinite loop incoming :(\n");
+	
 	i=0;
 
-	/*while (i<10)*/
+	/*while ( i < 10)*/
 	/*{*/
 	/*  putchar(i);*/
 	/*}*/
