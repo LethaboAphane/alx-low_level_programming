a83b8175f53522f22d6db8cdccd1ba967c88ebdc
LethaboAphane <lethabo_aphane@icloud.com>
2023-08-06 00:13:38 -0700

    to infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,29 +11,31 @@
 
 int main(int argc, char **argv)
 {
-	int i, n, sum=0;
+	int i, n, sum = 0;
 	char *flag;
-	if (argc <2)
+
+	if (argc < 2)
+
 {
 	printf("0\n");
 	return (0);
 }
-for (i =1; argv[i]; i++)
+for (i = 1; argv[i]; i++)
 
 {
-	n = strtol (argv[i], &flag, 10);
+	n = strtol(argv[i], &flag, 10);
 	if (*flag)
 	{ 
 		printf("Error\n");
-		return(1);
+		return (1);
 	}
 	else 
 	{
-		sum +=n;
+		sum += n;
 	}
 }
-                printf("%d\n" sum);
-		retun (0);
+                printf("%d\n", sum);
+	      return (0);
 }
 
 
