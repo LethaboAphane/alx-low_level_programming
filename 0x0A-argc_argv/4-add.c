8673a9cb57a027cb621b77d59015898d4913a4e7
LethaboAphane <lethabo_aphane@icloud.com>
2023-08-06 01:44:43 -0700

    numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdio.h>
-
 /**
  *main - adds positive numbers.
  * @argc: arguement count
@@ -12,32 +11,34 @@
 int main(int argc, char **argv)
 
 {
-	int i, n, sum = 0;
-	char *flag;
+        int i, n, sum = 0;
+        char *flag;
 
-	if (argc < 2)
+        if (argc < 2)
 
 {
-	printf("0\n");
-	return (0);
+        printf("0\n");
+        return (0);
 }
 for (i = 1; argv[i]; i++)
 
 {
-	n = strtol(argv[i], &flag, 10);
-	if (*flag)
-	{ 
-		printf("Error\n");
-		return (1);
-	}
-	else 
-	{
-		sum += n;
-	}
+        n = strtol(argv[i], &flag, 10);
+        if (*flag)
+        {
+                printf("Error\n");
+                return (1);
+        }
+        else
+        {
+                sum += n;
+        }
 }
               printf("%d\n", sum);
-	      return (0);
+              return (0);
 }
 
 
 
+~                                                                                                                                                                                                                  
+~                           
